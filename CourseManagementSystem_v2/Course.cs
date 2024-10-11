40c402d3f9fab8627a6c4c5f2bde60c58cbec26e
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-10-11 15:55:46 +0530

    Question 10 & 11 finished

---
+++ CourseManagementSystem_v2/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v2
+{
+    internal class Course
+    {
+        public string CourseId { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
