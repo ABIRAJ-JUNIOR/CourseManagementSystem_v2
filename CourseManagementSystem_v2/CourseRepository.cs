118d5360b400ea49c5d71120a3fc3b617abe6bd7
ABIRAJ-JUNIOR <abirajjr2002@gmail.com>
2024-10-11 16:05:55 +0530

    Question 13 INSERT finished

---
+++ CourseManagementSystem_v2/CourseRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v2
+{
+    internal class CourseRepository
+    {
+        private readonly string _connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=CourseManagement;Trusted_Connection=true;TrustServerCertificate=true;";
+
+        public void Insert(string id, string title, string duration, decimal price)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = @"INSERT INTO Courses(CourseId,Title,Duration,Price)VALUES(@courseId,@title,@duration,@price)";
+                    command.Parameters.AddWithValue("@courseId", id);
+                    command.Parameters.AddWithValue("@title", title);
+                    command.Parameters.AddWithValue("@duration", duration);
+                    command.Parameters.AddWithValue("@price", price);
+                    command.ExecuteNonQuery();
+
+                    Console.WriteLine($"\nCourse added successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR : {ex.Message}");
+            }
+        }
+
+       
+    }
+}
